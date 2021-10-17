8609124539fff4acad4bc1e57e5773de0ad17b5a
Conrado Pinheiro <conradopsa@gmail.com>
2021-10-17 17:14:21 -0300

    fix: :bug: fix dot env not found

--- db_repository/db_repository.go
+++ db_repository/db_repository.go
@@ -3,17 +3,47 @@ package db_repository
 import (
 	"context"
 	"log"
+	"path/filepath"
+	"runtime"
 
 	"github.com/joho/godotenv"
 	"github.com/monitprod/db_repository/pkg/loaders/database"
 )
 
-func StartRepository(ctx context.Context) {
-	err := godotenv.Load(".env")
+func StartRepositoryEnv(ctx context.Context, envPath string) {
+
+	if envPath == "" {
+		envPath = getDefaultEnvPath()
+	}
+
+	err := godotenv.Load(envPath)
 
 	if err != nil {
-		log.Fatalln("Error: Dot env not initialized")
+		log.Fatalln("Error: Dot env not initialized:", err)
 	}
 
 	database.ConnectClient(ctx)
+}
+
+func StartRepository(ctx context.Context) {
+
+	err := godotenv.Load(getDefaultEnvPath())
+
+	if err != nil {
+		log.Fatalln("Error: Dot env not initialized:", err)
+	}
+
+	database.ConnectClient(ctx)
+}
+
+func getDefaultEnvPath() string {
+	return getBasepath() + "\\.env"
+}
+
+func getBasepath() string {
+	var (
+		_, b, _, _ = runtime.Caller(0)
+		basepath   = filepath.Dir(b)
+	)
+	return basepath
 }
