9f7b388d5b5031670bff235b5fe65b014ed11114
conradopsa <conradopsa@gmail.com>
2021-10-25 01:28:20 -0300

    refactor: :recycle: Separating integration tests

---
+++ core/pkg/service/function_integration_test.go
@@ -0,0 +1,43 @@
+// +build integration
+
+package service
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/monitprod/core"
+)
+
+func TestExecLambda(t *testing.T) {
+	var payloadMock = map[string]interface{}{
+		"execution":         1,
+		"usersPerExecution": 1,
+	}
+
+	core.UseCoreSmp(func(ctx context.Context) {
+		var builder FunctionBuilder = FunctionBuilder{
+			Payload: payloadMock,
+			IsLocal: false,
+			ServiceOptions: &ServiceOptions{
+				Region:       os.Getenv("CORE_AWS_REGION"),
+				FunctionName: os.Getenv("CORE_AWS_FUNCTION_NAME"),
+				Credentials: Credentials{
+					AccessKey: os.Getenv("CORE_AWS_ACCESS_KEY_ID"),
+					Secret:    os.Getenv("CORE_AWS_SECRET"),
+					Token:     os.Getenv("CORE_AWS_TOKEN"),
+				},
+			},
+		}
+
+		funcService := NewFunctionServiceImp(builder)
+
+		err := funcService.Exec()
+		if err != nil {
+			log.Fatalln("Error while execute new FaaS function:", err)
+		}
+	})
+
+}
--- core/pkg/service/function_test.go
+++ core/pkg/service/function_test.go
@@ -1,25 +1,22 @@
 package service
 
 import (
-	"context"
 	"log"
-	"os"
 	"testing"
 
-	"github.com/monitprod/core"
 	"github.com/monitprod/core/pkg/util/local"
 )
 
-var payloadMock = map[string]interface{}{
-	"execution":         1,
-	"usersPerExecution": 1,
-}
-
-var localFuncMock local.LocalFunc = func(payload *map[string]interface{}) {
-	// My func. . .
-}
-
 func TestExecLocal(t *testing.T) {
+
+	var payloadMock = map[string]interface{}{
+		"execution":         1,
+		"usersPerExecution": 1,
+	}
+
+	var localFuncMock local.LocalFunc = func(payload *map[string]interface{}) {
+		// My func. . .
+	}
 
 	var builder FunctionBuilder = FunctionBuilder{
 		Payload:        payloadMock,
@@ -34,32 +31,5 @@ func TestExecLocal(t *testing.T) {
 	if err != nil {
 		log.Fatalln("Error while execute new FaaS function:", err)
 	}
-
-}
-
-func TestExecLambda(t *testing.T) {
-
-	core.UseCoreSmp(func(ctx context.Context) {
-		var builder FunctionBuilder = FunctionBuilder{
-			Payload: payloadMock,
-			IsLocal: false,
-			ServiceOptions: &ServiceOptions{
-				Region:       os.Getenv("CORE_AWS_REGION"),
-				FunctionName: os.Getenv("CORE_AWS_FUNCTION_NAME"),
-				Credentials: Credentials{
-					AccessKey: os.Getenv("CORE_AWS_ACCESS_KEY_ID"),
-					Secret:    os.Getenv("CORE_AWS_SECRET"),
-					Token:     os.Getenv("CORE_AWS_TOKEN"),
-				},
-			},
-		}
-
-		funcService := NewFunctionServiceImp(builder)
-
-		err := funcService.Exec()
-		if err != nil {
-			log.Fatalln("Error while execute new FaaS function:", err)
-		}
-	})
 
 }
